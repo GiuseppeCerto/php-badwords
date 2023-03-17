998f012d154801558116be0996c47c5382d0b454
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-03-17 17:01:51 +0100

    EDIT error undefined variable

---
+++ censored.php
@@ -0,0 +1,9 @@
+<?php 
+
+$name = $_GET['text'];
+var_dump($_GET);
+
+
+?>
+
+<p>Ciao <?php echo $text ?></p>
---
+++ index.php
@@ -0,0 +1,4 @@
+<form action="censored.php" method="get">
+<input type="paragraph" name="text" placeholder="Inserisci del testo">
+<button type="submit">Invia</button>
+</form>
